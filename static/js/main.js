6ee23772ac4b2f23e5f8bdab575c507aa9c279bf
gaudi <craig@resin.io>
2016-03-15 16:55:45 +0000

    remove screen-cast

--- static/js/main.js
+++ static/js/main.js
@@ -50,14 +50,6 @@ $(function() {
   });
 });
 
-//
-var iframe = $('#vimeo-player')[0];
-var player = $f(iframe);
-
-$('.videoModal').on('hidden.bs.modal', function () {
-    player.api('pause');
-})
-
 function cosmetics() {
   setTimeout(function(){
   $('.dropdown-menu').each(function(){
