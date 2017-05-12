5bf6b5657f1e51e32b036baf8693a86b6c5b6ead
craig-mulligan <craig@resin.io>
2017-05-12 15:00:37 +0200

    add experimental warning

--- pages/cli.js
+++ pages/cli.js
@@ -20,8 +20,11 @@ const Page = ({ content, downloads }) => (
     <div className="container">
       <div className="row">
         <div className="my-5 col-md-8 offset-md-2">
-          <h1>Etcher CLI - <code>experimental</code></h1>
+          <h1>Etcher CLI </h1>
           <p>The Etcher CLI is a command-line tool that aims to provide all the benefits of the Etcher desktop application in a way that can be run from a terminal, or even used from a script.</p>
+          <p>
+            <span className="badge badge-warning">Warning</span> the CLI is currently experimental, proceed with caution and <a href="https://github.com/resin-io/etcher/issues">report issues.</a>
+          </p>
           <h2 id="download">Download</h2>
           <Table items={downloads.links} />
           <h2 id="install">Install</h2>
