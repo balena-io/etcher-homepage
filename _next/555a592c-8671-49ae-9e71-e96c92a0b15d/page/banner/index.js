c3e325ae8468d50007a3f25fd50fb7e03b7ca5df
Shou <x@shou.io>
2017-06-26 12:59:50 +0100

    Updates

--- _next/555a592c-8671-49ae-9e71-e96c92a0b15d/page/banner/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/banner', function() {
-            var comp = module.exports=webpackJsonp([7],{415:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var l=n(1),u=function(e){return e&&e.__esModule?e:{default:e}}(l),o=function(e){e.content;return u.default.createElement("div",{className:"container"},u.default.createElement("p",null,"Hello world!"))};t.default=o},938:function(e,t,n){e.exports=n(415)}},[938]);
-            return { page: comp.default }
-          })
-        
