8fd2dba8d727b8cc8cb31b5c84ed7437429ef35b
Benedict Aas <x+g@shou.io>
2017-06-26 12:55:08 +0100

    feat: add first success-screen banner (#64)
    * feat: add first success-screen banner
    
    We add the first success-screen banner for Etcher, including buttons to
    star and tweet the project.
    
    Changelog-Entry: Add the first success-screen banner, with star and
    tweet buttons.

---
+++ pages/success-banner/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import Head from 'next/head';
+
+class Button extends React.Component {
+  render() {
+    return (
+      <a
+        href={ this.props.href }
+        target="_blank"
+        className="button">
+        { this.props.children }
+      </a>
+    )
+  }
+}
+
+const Banner = () => (
+  <main className="vertical center">
+    <div>
+      <h1>We hope you enjoyed using
+        <img src="/static/etcher.svg" className="etcher-logo" />
+      </h1>
+    </div>
+    <div className="horizontal center">
+      <Button href="https://github.com/resin-io/etcher">
+        <img className="icon github" src="/static/social/octocat.png" />
+        Star on Github
+      </Button>
+      <Button href="https://twitter.com/intent/tweet?text=%23etcher">
+        <img className="icon twitter" src="/static/social/twitter.png" />
+        Tweet
+      </Button>
+    </div>
+  </main>
+)
+
+const Footer = () => (
+  <footer>
+    made with
+    <img className="icon" src="/static/love.svg" />
+    by
+    <img className="brand" src="/static/resin.png" />
+  </footer>
+)
+
+const Page = ({ content }) => (
+  <html>
+    <Head>
+      <meta charset="utf-8" />
+      <link rel="stylesheet" type="text/css" href="/static/success-banner.css" />
+    </Head>
+    <body>
+      <Banner />
+      <Footer />
+    </body>
+  </html>
+)
+
+export default Page;
