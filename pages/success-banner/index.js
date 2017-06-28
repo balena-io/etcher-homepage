42d80d50f0097eab18c1a622b9c09c38d3ee42f3
Shou <x@shou.io>
2017-06-28 08:49:30 +0100

    feat: add analytic events
    We add analytics sent over the `console.log` method and intercepted by
    Etcher. The debug messages are in the [robot][1] format, e.g.
    
    ```js
    {"command":"log","data":"Hello, World!"}
    ```
    
    We also remove the recently introduced way to copy Javascript-less pages
    into the build because all banners will now contain code for analytics.
    
    [1]: https://github.com/resin-io/etcher/tree/master/lib/shared/robot#the-robot-mechanism

--- pages/success-banner/index.js
+++ pages/success-banner/index.js
@@ -1,91 +1,33 @@
 import React from 'react';
 import Head from 'next/head';
 
-/**
- * @summary Make and return an event method for a given event description string
- * @function
- * @private
- *
- * @param {String} eventDecs - event description
- *
- * @example
- * eventLog('click Tweet button');
- * > 'Banner click Tweet button'
- */
 const eventLog = (eventDesc) => {
   const data = 'Banner ' + eventDesc;
 
   return () => {
-    const url = new URL(location.href);
+    const logJSON = JSON.stringify({
+      command: 'log',
+      data
+    });
 
-    // Use the 'new' API version for more recent versions of Etcher utilizing
-    // the Robot object format.
-    if (url.searchParams.get('api-version') === '1') {
-      console.log(JSON.stringify({
-        command: 'log',
-        data
-      }));
-
-    // Fallback to the old pure-string format.
-    } else {
-      console.log(data);
-    }
-  };
-};
-
-/**
- * @summary Pure text links
- * @class
- * @private
- *
- * @example
- * <Link label="Example" href="https://example.com/">Example page</Link>
- */
-class Link extends React.PureComponent {
-  constructor() {
-    super();
-
-    this.type = 'link';
+    console.log(logJSON);
   }
+}
 
+class Button extends React.Component {
   render() {
     return (
       <a
         href={ this.props.href }
         target="_blank"
-        rel="noopener noreferrer"
-        className={ this.type }
-        onClick={ eventLog(`click ${this.props.label} ${this.type}`) }>
+        className="button"
+        onClick={ eventLog(this.props.event) }>
         { this.props.children }
       </a>
     )
   }
 }
 
-/**
- * @summary Button links
- * @class
- * @private
- *
- * @example
- * <Button label="Submit" href="/submit/">Submit something</Button>
- */
-class Button extends Link {
-  constructor() {
-    super();
-
-    this.type = 'button';
-  }
-}
-
-/**
- * @summary Banner
- * @function
- * @private
- *
- * @example
- * <Banner />
- */
 const Banner = () => (
   <main className="vertical center">
     <div>
@@ -95,13 +37,12 @@ const Banner = () => (
     </div>
     <div className="horizontal center">
       <Button href="https://github.com/resin-io/etcher"
-        label="star on Github">
+        event="click Github star button">
         <img className="icon github" src="/static/social/octocat.png" />
         Star on Github
       </Button>
-      <Button
-        href="https://twitter.com/intent/tweet?text=Just%20flashed%20an%20image%20with%20%23etcher%20https%3A%2F%2Fetcher.io%20by%20%40resin_io%20and..."
-        label="Tweet">
+      <Button href="https://twitter.com/intent/tweet?text=%23etcher"
+        event="click Tweet button">
         <img className="icon twitter" src="/static/social/twitter.png" />
         Tweet
       </Button>
@@ -109,35 +50,18 @@ const Banner = () => (
   </main>
 )
 
-/**
- * @summary Footer
- * @function
- * @private
- *
- * @example
- * <Footer />
- */
 const Footer = () => (
   <footer>
     made with
     <img className="icon" src="/static/love.svg" />
     by
-    <Link href="https://resin.io/"
-      label="Resin">
+    <a href="https://resin.io/">
       <img className="brand" src="/static/resin.png" />
-    </Link>
+    </a>
   </footer>
 )
 
-/**
- * @summary Page
- * @function
- * @public
- *
- * @example
- * <Page />
- */
-const Page = () => (
+const Page = ({ content }) => (
   <div>
     <Head>
       <meta charset="utf-8" />
