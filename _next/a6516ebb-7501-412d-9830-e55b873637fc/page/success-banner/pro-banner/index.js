e14489fd511b91a2a35f888b461c4d9cf9569d29
craig-mulligan <craig@resin.io>
2017-11-13 15:08:00 +0000

    Updates

---
+++ _next/a6516ebb-7501-412d-9830-e55b873637fc/page/success-banner/pro-banner/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/success-banner/pro-banner', function() {
+            var comp = module.exports=webpackJsonp([7],{381:function(o,t,e){"use strict"},944:function(o,t,e){o.exports=e(381)}},[944]);
+            return { page: comp.default }
+          })
+        
