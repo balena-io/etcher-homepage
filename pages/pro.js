f7b48f488dfc61bb47943fe8ce98078d9d684b72
craig-mulligan <craig@resin.io>
2017-11-24 11:20:56 +0000

    add linting

--- pages/pro.js
+++ pages/pro.js
@@ -7,13 +7,12 @@ import Subscribe from '../components/pro/Subscribe';
 import Description from '../components/pro/Description';
 import Jumbotron from '../components/pro/Jumbotron';
 import Features from '../components/pro/Features';
-import Waypoint from 'react-waypoint';
 import get from 'lodash/get';
 
-const Content = ({ children, navHide, toggleNav }) => {
+const Content = ({ children, toggleNav }) => {
   return (
     <Layout {...locals}>
-      <Nav hide={navHide}>
+      <Nav>
         <Subscribe action={locals.proMailChimpList} />
       </Nav>
       <Jumbotron mailChimpAction={locals.proMailChimpList} />
@@ -22,48 +21,33 @@ const Content = ({ children, navHide, toggleNav }) => {
   );
 };
 
-class Page extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {
-      navHide: true
-    };
+const Page = () => {
+  const variant = get(this.props, 'query.vn') || 'a';
+  switch (variant) {
+    case 'b':
+      return (
+        <Content navHide={this.state.navHide} toggleNav={this.toggleNav}>
+          <Features cols={2} features={locals.proFeatures} />
+          <Rationale />
+        </Content>
+      );
+      break;
+    case 'c':
+      return (
+        <Content navHide={this.state.navHide} toggleNav={this.toggleNav}>
+          <Rationale />
+        </Content>
+      );
+      break;
+    default:
+      return (
+        <Content navHide={this.state.navHide} toggleNav={this.toggleNav}>
+          <Description />
+          <Features cols={2} features={locals.proFeatures} />
+        </Content>
+      );
   }
-
-  toggleNav = newState => {
-    this.setState({
-      navHide: newState
-    });
-  };
-
-  render() {
-    const variant = get(this.props, 'query.vn') || 'a';
-    switch (variant) {
-      case 'b':
-        return (
-          <Content navHide={this.state.navHide} toggleNav={this.toggleNav}>
-            <Features cols={2} features={locals.proFeatures} />
-            <Rationale />
-          </Content>
-        );
-        break;
-      case 'c':
-        return (
-          <Content navHide={this.state.navHide} toggleNav={this.toggleNav}>
-            <Rationale />
-          </Content>
-        );
-        break;
-      default:
-        return (
-          <Content navHide={this.state.navHide} toggleNav={this.toggleNav}>
-            <Description />
-            <Features cols={2} features={locals.proFeatures} />
-          </Content>
-        );
-    }
-  }
-}
+};
 
 Page.getInitialProps = ({ query }) => {
   return { query };
